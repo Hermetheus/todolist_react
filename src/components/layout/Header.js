a339b1fb6fa5fc96a3313d43bacc37a3db8b6f10
Hermetheus <allanmolson@gmail.com>
2019-08-07 22:32:49 -0500

    ToDoList

---
+++ src/components/layout/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header style={headerStyle}>
+      <Link to="/" style={{ color: "#fff", size: "24px" }}>
+        <h1>Todo List</h1>
+      </Link>
+      <Link to="/" style={linkStyle}>
+        Home
+      </Link>{" "}
+      |{" "}
+      <Link to="/about" style={linkStyle}>
+        About
+      </Link>
+    </header>
+  );
+}
+
+const headerStyle = {
+  background: "#333",
+  color: "#fff",
+  textAlign: "center",
+  padding: "10px"
+};
+
+const linkStyle = {
+  color: "#fff",
+  textDecoration: "none"
+};
